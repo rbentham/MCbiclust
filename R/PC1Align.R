aecef792b6c7d7678860fe5eaee1513c4f759e59
rbentham <robert.bentham89@gmail.com>
2016-08-19 14:19:18 +0100

    Small changes to PC1Align and vignette

--- R/PC1Align.R
+++ R/PC1Align.R
@@ -3,15 +3,18 @@
 #' @param gem Gene expression matrix
 #' @param PC1 First principal component
 #' @param CV Correlation vector
+#' @param sort.order Order of samples
 #' @param bic bicluster
 #' @return Aligned PC1 vector
 #' @export
 
 
-PC1Align <- function(gem, PC1, CV, bic){
+PC1Align <- function(gem, PC1, CV, sort.order, bic){
   
   max.cv.loc <- which.max(CV)
-  cor.test <- cor(gem[max.cv.loc, bic[[2]]], PC1[seq(length = bic[[2]])])
+  number.samps <- seq(length = bic[[2]])
+  gem1 <- gem[max.cv.loc, sort.order[number.samps]]
+  cor.test <- cor(gem1, PC1[number.samps])
   
   if(cor.test < 0){
     PC1 <- -PC1
