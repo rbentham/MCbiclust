4dd518a0080fd3990f10386267f2f516ef2f7db0
rbentham <robert.bentham89@gmail.com>
2016-08-24 14:25:26 +0100

    Fixed bug in PC1Align and new ForkClassifier function

--- R/PC1Align.R
+++ R/PC1Align.R
@@ -12,9 +12,9 @@
 PC1Align <- function(gem, PC1, CV, sort.order, bic){
   
   max.cv.loc <- which.max(CV)
-  number.samps <- seq(length = bic[[2]])
+  number.samps <- seq(length = length(bic[[2]]))
   gem1 <- gem[max.cv.loc, sort.order[number.samps]]
-  cor.test <- cor(gem1, PC1[number.samps])
+  cor.test <- cor(as.numeric(gem1), PC1[number.samps])
   
   if(cor.test < 0){
     PC1 <- -PC1
