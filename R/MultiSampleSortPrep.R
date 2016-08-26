99747acb300197bf123b41488e60f7f0caaa412c
rbentham <robert.bentham89@gmail.com>
2016-08-26 16:13:03 +0100

    New function MultiSampleSortPrep. Old function MultiSampleSort removed

---
+++ R/MultiSampleSortPrep.R
@@ -0,0 +1,35 @@
+#' Preparation for SampleSort for multiple patterns
+#' 
+#' @param gem Gene expression matrix
+#' @param av.corvec List of average correlation vector 
+#' @param top.genes.num Number of the top genes in correlation vector to use for sorting samples
+#' @param groups List showing what runs belong to which correlation vector group
+#' @param initial.seeds List of sample seeds from all runs
+#' @param num.cores Number of cores for use in parallel evaluation
+#' @param sort.length Number of samples to be sorted
+#' @return Order of samples by strength to correlation pattern
+#' @example example_code/example_sil.R
+#' @export
+
+MultiSampleSortPrep <- function(gem, av.corvec, top.genes.num, groups, initial.seeds){
+  
+  top.genes <- lapply(X = av.corvec,
+                      FUN = function(x) order(abs(x),decreasing = T)[seq(length = top.genes.num)])
+  
+  top.seed.score <- lapply(groups, FUN = function(x) seq(length = length(x)))
+  
+  top.seed.fun1 <- function(y) sapply(seq(length = length(groups[[y]])),
+                                          FUN = function(x){
+                                            l1 <- top.genes[[y]]
+                                            l2 <- initial.seeds[groups[[y]]][[x]]
+                                            return(mean(abs(cor(t(as.matrix(gem)[l1,l2])))))
+                                            })
+  
+  top.seed.score <- lapply(seq(length = length(groups)),FUN = top.seed.fun1)
+  
+  top.seed <- lapply(seq(length = length(av.corvec)),
+                     FUN = function(x) initial.seeds[groups[[x]]][[which.max(top.seed.score[[x]])[1]]])
+  top.mat <- lapply(top.genes, FUN = function(x) as.matrix(gem)[x,])
+  
+  return(list(top.seed,top.mat))
+}
