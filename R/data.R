11ab36a63040dc7251616e9f05d3d5898634439d
rbentham <robert.bentham89@gmail.com>
2016-09-02 14:23:03 +0100

    Return value

--- R/data.R
+++ R/data.R
@@ -20,7 +20,7 @@
 #'   \item Name: Sample name identifier
 #' }
 #' @source \url{http://www.broadinstitute.org/ccle/data/browseData} Filename: CCLE_sample_info_file_2012-04-06.txt 
-#' @value NA
+#' @return NA
 "CCLE_samples"
 
 
@@ -36,7 +36,7 @@
 #'   \item ...
 #' }
 #'  @source \url{http://www.broadinstitute.org/ccle/data/browseData} Filename: CCLE_Expression_Entrez_2012-04-06.gct.gz
-#' @value NA
+#' @return NA
 "CCLE_small"
 
 
@@ -47,7 +47,7 @@
 #'
 #' @format A Character vector of the HGNC approved gene names
 #' @source \url{https://www.broadinstitute.org/publications/broad807s} 
-#' @value NA
+#' @return NA
 "Mitochondrial_genes"
 
 #' List of samples pre-sorted for vignette
@@ -55,6 +55,6 @@
 #' Different sortings of CCLE_small calcualted in the Vignette.
 #' 
 #' @format A list of the different sample sortings calculated in the vignette:
-#' @value NA
+#' @return NA
 "Vignette_sort"
 
